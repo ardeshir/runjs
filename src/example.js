c17a86c48b40b1a61e01295551f338ee9438f092
ardeshir sepahsalar <ardeshir.org@gmail.com>
2022-07-29 16:31:39 -0500

    adding console log & error

--- src/example.js
+++ src/example.js
@@ -1,3 +1,5 @@
 // example.js
 
 Deno.core.print("Hello runjs!\n");
+console.log("logging enabled!");
+console.error("error enabled!");
--- src/runtime.js
+++ src/runtime.js
@@ -1,17 +1,17 @@
 // runtime.js
-(( globalThis)) => {
+(( globalThis) => {
 	const core = Deno.core;
 
 	function argsToMessage (...args) {
-		return args.map((arg) => JSON.stringfy(arg)).join(" "):
+		return args.map((arg) => JSON.stringify(arg)).join(" ");
 	}
 
 	globalThis.console = {
-		log: (...args)  => {
+		log:   (...args) => {
 		  core.print(`[out]: ${argsToMessage(...args)}\n`, false);
 		},
 		error: (...args) => {
-		  core.print(`[err]: ${argsToMessage(...args)}\n', true);
+		  core.print(`[err]: ${argsToMessage(...args)}\n`, true);
 	    },
 	};
-	})(globalThis); 
+})(globalThis);
